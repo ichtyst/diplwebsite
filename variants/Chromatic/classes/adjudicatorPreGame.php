15bc696ce121d47b1e56a3f234ea8119300a9cbe
Oliver Auth <Oliver.Auth@rhoen.de>
2014-05-03 17:10:05 +0200

    New variant: Chromatic

---
+++ variants/Chromatic/classes/adjudicatorPreGame.php
@@ -0,0 +1,14 @@
+﻿<?php
+defined('IN_CODE') or die('This script can not be run by itself.');
+
+class ChromaticVariant_adjudicatorPreGame extends adjudicatorPreGame {
+
+	protected $countryUnits = array(
+		'Blue'           => array('Cobalt'=>'Army'  , 'Royal'=>'Army'  , 'Sapphire'=>'Fleet'),
+		'Yellow'         => array('Topaz'=>'Fleet'     , 'Gold'=>'Army'  , 'Sunshine'=>'Army'),
+		'Dark'      => array('Jet'=>'Army'     , 'Obsidian'=>'Fleet' , 'Ebony'=>'Army'),
+		'Light'    => array('Alabaster'=>'Fleet'   , 'Ghost'=>'Army'  , 'Ivory'=>'Army'),
+		'Red'       => array('Vermillion'=>'Fleet', 'Ruby'=>'Army'   , 'Crimson'=>'Army'),
+	);
+
+}
---
+++ variants/Chromatic/variant.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('IN_CODE') or die('This script can not be run by itself.');
+
+class ChromaticVariant extends WDVariant {
+	public $id         = 93;
+	public $mapID      = 93;
+	public $name       ='Chromatic';
+	public $fullName   = 'Chromatic';
+	public $description= 'A balanced 5 player variant';
+	public $author    = 'Jimmy Millington, Robs Schone and Lynsey Smith';
+	public $adapter    = 'Tristan';
+	public $homepage       ='http://dipwiki.com/index.php?title=Chromatic';
+	public $version    = '1';
+	public $codeVersion= '1.0';
+		
+	public $countries=array('Blue','Yellow','Dark','Light','Red');
+
+	public function __construct() {
+		parent::__construct();
+		$this->variantClasses['drawMap']            = 'Chromatic';
+		$this->variantClasses['adjudicatorPreGame'] = 'Chromatic';
+	}
+
+	public function turnAsDate($turn) {
+		if ( $turn==-1 ) return "Pre-game";
+		else return ( $turn % 2 ? "Autumn, " : "Spring, " ).(floor($turn/2) + 1901);
+	}
+
+	public function turnAsDateJS() {
+		return 'function(turn) {
+			if( turn==-1 ) return "Pre-game";
+			else return ( turn%2 ? "Autumn, " : "Spring, " )+(Math.floor(turn/2) + 1901);
+		};';
+	}
+}
+
+?>

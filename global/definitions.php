42690439d9bd619f991a6098fb235773c95b6e36
Oliver Auth <Oliver.Auth@rhoen.de>
2014-07-28 23:35:33 +0200

    New game-director-feature: block votes. (part 1, not completed, more work to be done later)

--- global/definitions.php
+++ global/definitions.php
@@ -25,7 +25,7 @@
 defined('IN_CODE') or die('This script can not be run by itself.');
 
 define("VERSION", 135) ;
-define("VDIPVERSION", 47);
+define("VDIPVERSION", 48);
 
 // Some integer values which are named for clarity.
 

2aba6d6d718374f0d48a75a3b5125b4f4073e9ce
ichtyst <info@ichtyst.de>
2015-01-30 11:47:56 +0100

    glob durchsucht 'rekursiv'

--- variants.php
+++ variants.php
@@ -34,7 +34,7 @@ if(!(isset($_REQUEST['variantID'])))
 	$variantsOn=array();
 	$variantsOff=array();
 
-	foreach(glob('variants/*') as $variantDir)
+	foreach(glob('variants/*', GLOB_ONLYDIR) as $variantDir)
 	{
 		if( file_exists($variantDir.'/variant.php') )
 		{
